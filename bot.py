0fff4d128d02f5580fe5721d93538bcfcffde36d
Pashckevich227 <iport234@gmail.com>
2022-07-24 00:02:55 +0300

    feat: persons_checks, add database in project(feat: new def check_users_in_bd)

--- bot.py
+++ bot.py
@@ -3,7 +3,123 @@ import sqlite3
 
 bot = telebot.TeleBot("5436229582:AAHBayc_KAdkIFyHNuF4rDNgVaX7uXw_9H8", parse_mode=None)
 
-count_galochka_Pashckevich = 0
+count_galochka_Pasha = 0
+count_galochka_Lesha = 0
+count_galochka_Dan = 0
+count_galochka_Artem = 0
+count_galochka_Dima = 0
+count_galochka_Ilia = 0
+count_galochka_Sania = 0
+count_galochka_Andrey = 0
+
+# Pasha_id = 0
+# Lesha_id = 2
+# Dan_id = 3
+# Artem_id = 4
+# Dima_id = 5
+# Ilia_id = 6
+# Sania_id = 7
+# Andrey_id = 8
+
+# База данных
+with sqlite3.connect("orders.db") as db:
+    sql = db.cursor()
+
+sql.execute("""CREATE TABLE IF NOT EXISTS users(
+           user_id INT PRIMARY KEY,
+           count_checks INT);
+        """)
+
+def insert_varible_into_table(user_id, count_checks):
+    global sql, db
+    try:
+        db = sqlite3.connect('orders.db')
+        sql = db.cursor()
+
+        db.execute("INSERT or IGNORE INTO users(user_id, count_checks) VALUES (?, ?)", (user_id, count_checks))
+        db.commit()
+        print(f"{user_id} {count_checks} вставлены в таблицу users")
+
+        sql.close()
+
+    except sqlite3.Error as error:
+        print("Ошибка при работе с SQLite", error)
+    finally:
+        sql.close()
+        db.close()
+        print("Соединение с SQLite закрыто")
+
+
+def delete_sqlite_record(user_id):
+    global sql, db
+    try:
+        db = sqlite3.connect('orders.db')
+        sql = db.cursor()
+        print("Подключен к SQLite")
+
+        sql_update_query = """DELETE from users where user_id = ?"""
+        sql.execute(sql_update_query, (user_id,))
+        db.commit()
+        print("Запись успешно удалена")
+        sql.close()
+
+    except sqlite3.Error as error:
+        print("Ошибка при работе с SQLite", error)
+    finally:
+        sql.close()
+        db.close()
+        print("Соединение с SQLite закрыто")
+
+
+def print_db():
+    global sql, db
+    try:
+        db = sqlite3.connect('orders.db')
+        sql = db.cursor()
+        print("Подключен к SQLite")
+
+        for _ in sql.execute("SELECT * FROM users;"):
+            print(sql.fetchall())
+
+        sql.close()
+
+    except sqlite3.Error as error:
+        print("Ошибка при работе с SQLite", error)
+    finally:
+        sql.close()
+        db.close()
+        print("Соединение с SQLite закрыто")
+
+def check_user_in_db(user_id):
+    global sql, db
+    try:
+        db = sqlite3.connect('orders.db')
+        sql = db.cursor()
+
+        info = sql.execute('SELECT * FROM users WHERE user_id=?', (user_id, ))
+        if info.fetchone() is None:
+            db.execute("INSERT INTO users(user_id) VALUES (?)", (user_id, ))
+            db.commit()
+            print('Добавил!')
+        else:
+            print(*sql.fetchall())
+            print('None')
+
+    except sqlite3.Error as error:
+        print("Ошибка при работе с SQLite", error)
+    finally:
+        sql.close()
+        db.close()
+        print("Соединение с SQLite закрыто")
+
+
+#delete_sqlite_record(i)
+#insert_varible_into_table(8, 0)
+#check_user_in_db(1)
+#print_db()
+
+
+# Функционал бота
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -14,14 +130,19 @@ def start(message):
 def help(message):
     bot.send_message(message.chat.id, f'По всем вопросам к кабану')
 
-@bot.message_handler(commands=['Галочку_Pashckevich'])
+@bot.message_handler(commands=['Галочку_Pasha'])
 def add(message):
-    global count_galochka_Pashckevich
-    count_galochka_Pashckevich += 1
+    global count_galochka_Pasha
+    count_galochka_Pasha += 1
     bot.send_message(message.chat.id, f"Присвоил {message.from_user.first_name} галочку")
 
 @bot.message_handler(commands=['stats'])
 def stats(message):
-    bot.send_message(message.chat.id, f'{message.from_user.first_name} {count_galochka_Pashckevich}')
+    bot.send_message(message.chat.id, f'{message.from_user.first_name} {count_galochka_Pasha}')
+
+#@bot.message_handler(commands=['info'])
+#def get_user_info(message):
+#    bot.send_message(message.chat.id, message)
 
 bot.polling(none_stop=True)
+
